7fd8e2b0d7e112513f02697b43667f73e2a2c0e5
Rio Adrian <riorasyid02@gmail.com>
2024-10-28 10:27:38 +0700

    lanjut

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+$db = mysqli_connect('localhost' , 'root' , '', 'crud-php');
+
+// cek koneksi
+
+// if (!$db) {
+//     echo 'gagal';
+// }
+// else {
+//     echo 'berhasil';
+// }
+
+?>
