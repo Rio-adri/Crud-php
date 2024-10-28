2115452e517a973847a3c9ef34b7fb1d1ae120db
Rio Adrian <riorasyid02@gmail.com>
2024-10-28 08:59:41 +0700

    video1

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Full Crud PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <div>
+        <nav class="navbar bg-dark navbar-expand-lg " data-bs-theme = "dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">CRUD PHP</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="index.php">Barang</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Mahasiswa</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Modal</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container mt-5">
+        <h1>Data Barang</h1>
+
+        <table class="table table-bordered table-striped mt-3">
+            <thead>
+                <tr>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Nama</th>
+                    <th class="text-center">Jumlah</th>
+                    <th class="text-center">Harga</th>
+                    <th class="text-center">Tanggal</th>
+                    <th class="text-center">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mouse</td>
+                    <td>5</td>
+                    <td>50000</td>
+                    <td>28/10/2024</td>
+                    <td width="15%" class="text-center">
+                        <a href="" class="btn btn-primary">Ubah</a>
+                        <a href="" class="btn btn-danger">Hapus</a>
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
