5cc6f2d5017e2e98ffa84f2e9bd3903ff9bdbb5a
Rio Adrian <riorasyid02@gmail.com>
2024-11-13 17:11:51 +0700

    Membuat Download Laporan PDF Di PHP

--- mahasiswa.php
+++ mahasiswa.php
@@ -39,6 +39,8 @@ $data_mahasiswa = select("SELECT * FROM mahasiswa ORDER BY id_mahasiswa DESC")
 
         <a href="download-excel-mahasiswa.php" class="btn btn-success mb-1"><i class="fas fa-file-excel"></i> Download Excel</a>
 
+        <a href="download-pdf-mahasiswa.php" class="btn btn-danger mb-1"><i class="fas fa-file-pdf"></i> Download PDF</a>
+
         <table class="table table-bordered table-striped mt-3" id="table">
             <thead>
                 <tr>
